bf7599865a4cfc8d0b5b28363ea59b6675fc8700
César Rodas <vinizio007@gmail.com>
2020-05-31 23:57:42 -0600

    Initial commit

---
+++ app/src/main/java/com/example/lab8/news/News.kt
@@ -0,0 +1,34 @@
+package com.example.lab8.news
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+
+
+import com.example.lab8.R
+
+class News : Fragment() {
+
+    companion object {
+        fun newInstance() = News()
+    }
+
+    private lateinit var viewModel: NewsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.news_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
